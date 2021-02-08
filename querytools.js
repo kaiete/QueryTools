c35f2c41ceabbdb771157b9d4920b161d5584e62
kaiete <kaiete@aragon-press.com>
2021-02-08 11:00:33 +0000

    Update querytools.js

--- querytools.js
+++ querytools.js
@@ -1,5 +1,12 @@
 var qt = {
   modify : function(id,part,to) {
     document.getElementById(id).part = to
+  },
+  ask : function(query) {
+    let askresponse = prompt(query)
+    return askresponse
+  },
+  say : function(msg) {
+    alert(msg)
   }
 }
