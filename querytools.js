09373729863a913c507438cfe0904a8fa8911590
kaiete <kaiete@aragon-press.com>
2021-02-12 17:31:31 +0000

    Update querytools.js

--- querytools.js
+++ querytools.js
@@ -11,5 +11,32 @@ var qt = {
   },
   log : function(msg) {
     console.log(msg)
+    return "undefined"
+  },
+  link : function(site) {
+    window.location.href=site
+  },
+  redir : function(site) {
+    window.location.replace(site)
+  },
+  params : function() {
+    let parameters = new URLSearchParams(location.search)
+    let editedparams = decodeURIComponent(parameters)
+    delete parameters
+    let params = editedparams
+    return params
+  },
+  if : function(statementToCheck,toRunIfTruthy,toRunIfFalsy) {
+    if (statementToCheck) {
+      return true
+      eval(toRunIfTruthy)
+    } else if (not statementToCheck) {
+      return false
+      eval(toRunIfFalsy)
+    } else {
+      return "QT-Error: returned neither true nor false"
+      alert("Please press Ctrl+shift+I (Cmd+shift+I), then tap Console")
+      console.log("QueryTools ran into an error, and this site might not work correctly. We recommend contacting the developer and telling them that QueryTools found an invalid qt.if statement.")
+    }
   }
 }
